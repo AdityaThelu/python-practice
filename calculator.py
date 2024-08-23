5dcc2fbecd8d489e40113d4dc59526bfc3d337c0
Thelu Mohan Purushottam Aditya <official.thelu@gmail.com>
2024-08-23 11:26:49 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,11 @@
+n = int(input())
+if n==299 or 499 or 699:
+#for 1 mobile/1 laptop
+    if n==299:
+        print(299*12)
+#for 2 mobile/ 1 laptop
+    elif n==499:
+        print(499*12)
+#for 2 mobile/ 2 laptop
+    elif n==699:
+        print(699*12)
